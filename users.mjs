a8d66262e2e8b0ac4ca2fd5481bf83cc5bbab017
tim <timabrest111@gmail.com>
2025-03-23 18:41:31 +0300

    Create users.mjs

---
+++ users.mjs
@@ -0,0 +1,105 @@
+const users = [
+    {
+      id:"1",
+      name: "Alice Johnson",
+      description: "Frontend Developer",
+      avatar: "https://randomuser.me/api/portraits/women/1.jpg",
+      role: "Admin",
+      price:1000,
+      featured:"true",
+      gender:"female"
+    },
+    {
+      id:"2",
+      name: "Michael Smith",
+      description: "Backend Engineer",
+      avatar: "https://randomuser.me/api/portraits/men/2.jpg",
+      role: "User",
+      price:1100,
+      featured:"false",
+      gender:"male"
+    },
+    {
+      id:"3",
+      name: "Emma Williams",
+      description: "UI/UX Designer",
+      avatar: "https://randomuser.me/api/portraits/women/3.jpg",
+      role: "Moderator",
+      price:1100,
+      featured:"true",
+      gender:"female"
+    },
+    {
+      id:"4",
+      name: "Daniel Brown",
+      description: "Full Stack Developer",
+      avatar: "https://randomuser.me/api/portraits/men/4.jpg",
+      role: "User",
+      price:1300,
+      featured:"false",
+      gender:"male"
+    },
+    {
+      id:"5",
+      name: "Sophia Martinez",
+      description: "Product Manager",
+      avatar: "https://randomuser.me/api/portraits/women/5.jpg",
+      role: "Admin",
+      price:1500,
+      featured:"true",
+      gender:"female"
+    },
+    {
+      id:"6",
+      name: "Liam Garcia",
+      description: "Cloud Engineer",
+      avatar: "https://randomuser.me/api/portraits/men/6.jpg",
+      role: "User",
+      price:1040,
+      featured:"true",
+      gender:"male"
+    },
+    {
+      id:"7",
+      name: "Olivia Anderson",
+      description: "Security Analyst",
+      avatar: "https://randomuser.me/api/portraits/women/7.jpg",
+      role: "Moderator",
+      price:1200,
+      featured:"false",
+      gender:"female"
+    },
+    {
+      id:"8",
+      name: "James Taylor",
+      description: "Machine Engineer",
+      avatar: "https://randomuser.me/api/portraits/men/8.jpg",
+      role: "User",
+      price:1030,
+      featured:"true",
+      gender:"male"
+    },
+    {
+      id:"9",
+      name: "Isabella Thomas",
+      description: "Data Scientist",
+      avatar: "https://randomuser.me/api/portraits/women/9.jpg",
+      role: "Admin",
+      price:2100,
+      featured:"false",
+      gender:"female"
+    },
+    {
+      id:"10",
+      name: "Ethan White",
+      description: "DevOps Engineer",
+      avatar: "https://randomuser.me/api/portraits/men/10.jpg",
+      role: "User",
+      price:1000,
+      featured:"true",
+      gender:"male"
+    }
+   
+  ]
+  
+  export default users 
